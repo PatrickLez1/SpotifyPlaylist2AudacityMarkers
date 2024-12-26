dd31ff821a9fcf271ecc4382b7fe9abf94170ed1
PatrickLez1 <44232816+PatrickLez1@users.noreply.github.com>
2024-12-26 21:59:32 +0100

    Initial commit

---
+++ GetTitlesInfoFromPLaylist.R
@@ -0,0 +1,63 @@
+#previously to using read de ReadMe. You need to create a Spotify Developper account.
+
+#library
+library(jsonlite,httr)
+
+#parameters
+playlistID<-"5EpBGuaHk34P1Z01GBIFLK" #Put here the playlist ID as it appears in the url of the spotify playlist
+saveDirectory<-"." #directory where to save the .txt files with the audacity markers
+client_id<-""      #Spotify for developers  client_id
+client_secret<-""  #Spotify for developers  client_secret
+market<-"FR"       #Spotify market indentification. It helps if songs in the playlist are not available in your area.
+
+
+#####code
+
+# Obtain the access token
+response <- httr::POST(
+  url = "https://accounts.spotify.com/api/token",
+  body = list(
+    grant_type = "client_credentials",
+    client_id = client_id,
+    client_secret = client_secret
+  ),
+  encode = "form"
+)
+
+# Extract the token from the response
+token <- httr::content(response)$access_token
+
+# Define the headers with the token
+headers <- httr::add_headers(Authorization = paste("Bearer", token))
+
+# get playlist general info to find the title
+resTitle <- httr::GET(
+  url = paste0("https://api.spotify.com/v1/playlists/", playlistID, "?market=",market),
+  headers
+)
+resTitle_content <- httr::content(resTitle, as = "text", encoding = "UTF-8")
+title<-jsonlite::fromJSON(resTitle_content)$name 
+
+# get tracks info to get tracks title, artists and duration
+res <- httr::GET(
+  url = paste0("https://api.spotify.com/v1/playlists/", playlistID, "/tracks"),
+  headers
+)
+response_content <- httr::content(res, as = "text", encoding = "UTF-8")
+
+#construct the markers .txt file as expected by audacity
+json<-jsonlite::fromJSON(response_content)
+data<-json[["items"]]$track
+data<-data[,c("name","artists","duration_ms")]
+startYT=0
+stopYT=0
+file.create(paste0(saveDirectory,"/",title,".txt"))
+for(k in 1:nrow(data)){
+  X<-data[k,]
+  ar<-paste(X$artists[[1]]$name,collapse=",")
+  titleYoto<-paste0(X$name,"-",ar)
+  stopYT<-startYT+X$duration_ms/1000
+  nltxt<-paste0(startYT,"	",stopYT,"	",titleYoto)
+  write(nltxt, file = paste0(title,".txt"), append = TRUE)
+  startYT=stopYT
+}
--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # SpotifyPlaylist2AudacityMarkers
 A small script to request information of a spotify playlist via its API, in order to create a marker txt file delimiting marker for Audacity.
+It does note take care of ad interludes. So you might need a premium spotify account or to manually make the adjustment in Audacity.
+
+#Spotify credential
+To be able to run the script you need spotify for developers credential.
+Visit https://developer.spotify.com/, go to your Dashboard and click on "Create app".
+* The App name and App description doesn't matter here. Put anything.
+* For the redirect Redirect URIs, you can choose any valide adresse. For instance, https://www.google.fr hworks.
+* Tick Web API for the "Which API/SDKs are you planning to use?" section.
+
+Once the app saved, go bask to the dashboard and open the newly created app.
+In the settings, in the first section you will find the Client ID. Click on View client secret to get the Client secret value.
+These two values need to be copy and paste in the R code.
+
+#run the code
+To run the code, you will need to install the libraries jsonlite and httr.
+In the R script, fill the required parameters.
+The result file will be saved in the directory given as a parameter of the script. The name of the file is the name of the playlist.
+
+#usage of Audacity
+This script is intended to be used as a help when making a legal private copy of a spotify playlist.
+It means :
+* open audacity
+* choose the source of the sound
+* press the record button
+* press play on spotify to listen to the playlist. Audacity will not start to record if their is no song or music.
+* when the playlist is over, stop the recording.
+* Then import the marker file. The marker should be aligned with the start and the end of each song.
+* Finaly export the song as mp3 with file names based on the markers.
